44799c79f40c3b071bff1fa0ee4e8552ef7b51ea
Ons Mhiri <onsmhiri123@gmail.com>
2026-05-01 12:02:31 +0100

    add USerMApper

--- src/main/java/com/example/projet/mappers/UserMapper.java
+++ src/main/java/com/example/projet/mappers/UserMapper.java
@@ -1,12 +1,17 @@
 package com.example.projet.mappers;
 
-import com.example.projet.entities.User;
+import com.example.projet.dto.CreateUserRequest;
 import com.example.projet.dto.UserDTO;
+import com.example.projet.entities.Admin;
+import com.example.projet.entities.ClinicAdmin;
+import com.example.projet.entities.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
+
     public UserDTO toDTO(User user) {
+        if (user == null) return null;
         UserDTO dto = new UserDTO();
         dto.setId(user.getId());
         dto.setFirstName(user.getFirstName());
@@ -16,5 +21,27 @@ public class UserMapper {
         dto.setRole(user.getRole());
         dto.setCreatedAt(user.getCreatedAt());
         return dto;
+    }
+
+    public Admin toAdminEntity(CreateUserRequest request) {
+        if (request == null) return null;
+        Admin admin = new Admin();
+        admin.setFirstName(request.getFirstName());
+        admin.setLastName(request.getLastName());
+        admin.setEmail(request.getEmail());
+        admin.setPasswordHash(request.getPassword());
+        admin.setPhone(request.getPhone());
+        return admin;
+    }
+
+    public ClinicAdmin toClinicAdminEntity(CreateUserRequest request) {
+        if (request == null) return null;
+        ClinicAdmin clinicAdmin = new ClinicAdmin();
+        clinicAdmin.setFirstName(request.getFirstName());
+        clinicAdmin.setLastName(request.getLastName());
+        clinicAdmin.setEmail(request.getEmail());
+        clinicAdmin.setPasswordHash(request.getPassword());
+        clinicAdmin.setPhone(request.getPhone());
+        return clinicAdmin;
     }
 }
