34dc460742a283618975cb1e82e5a8e8abc332f7
emna <mayna>
2026-05-03 13:28:32 +0100

    updated security config

--- src/main/java/com/example/projet/config/SecurityConfig.java
+++ src/main/java/com/example/projet/config/SecurityConfig.java
@@ -1,18 +1,19 @@
-package com.example.projet.config; // same package as your CorsConfig
+package com.example.projet.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeRequests()
-            .anyRequest().permitAll(); 
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()  // allow all requests
+            );
+        return http.build();
     }
 }
